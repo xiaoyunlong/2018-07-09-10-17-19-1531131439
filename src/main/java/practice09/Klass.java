d9042f864b435e5ac45b7c090215a5f036f6dd21
xiaoyunlong <1758217010@qq.com>
2020-07-22 23:07:00 +0800

    [Lebron] Feature :do the pratice9,and don't approve test ,it told me that my practice04 have many problem

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,39 @@
 package practice09;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void assignLeader(Student student){
+        if (student.getKlass().getNumber()!=this.getNumber()){
+            System.out.println("It is not one of us");
+            return;
+        }
+
+        this.leader = student;
+    }
+
+    public String getDisplayName(){
+        return "Class "+ this.getNumber();
+    }
+
+    public void appendMember(Student student){
+        student.getKlass().setNumber(this.getNumber());
+    }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,24 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person{
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age, klass);
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public String introduce(){
+        if (this.getKlass() != null){
+            return super.introduce()+" I am a Teacher. I teach Class "+this.getKlass().getNumber()+".";
+        }
+        return super.introduce()+" I am a Teacher. I teach No Class.";
+    }
+    public String introduceWith(Student student){
+        if(student.getKlass().getNumber() == this.getKlass().getNumber()){
+            return super.introduce()+" I am a Teacher. I teach "+student.getName()+".";
+        }
+        return super.introduce()+" I am a Teacher. I don't teach "+student.getName()+".";
+    }
 }
