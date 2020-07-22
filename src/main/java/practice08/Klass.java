739dc6f4c058f5ef891b912e1daf570b8f42dbeb
xiaoyunlong <1758217010@qq.com>
2020-07-22 22:35:00 +0800

    [Lebron] Feature: done the Pratice08, but I still have a problem at Practice08Test.java:65

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,26 @@
 package practice08;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void assignLeader(Student student){
+        this.leader = student;
+    }
+
+    public String getDisplayName(){
+        return "Class "+ this.getNumber();
+    }
 }
--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,51 @@
 package practice08;
 
 public class Person {
+    protected int id;
+    protected String name;
+    protected int age;
+    protected Klass klass;
+
+    public Person(int id, String name, int age, Klass klass) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.klass = klass;
+    }
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String introduce(){
+        return "My name is "+this.getName() + ". I am "+ this.getAge()+" years old.";
+    }
+
+    @Override //不能修改参数列表
+    public boolean equals(Object obj) {
+       if(this.getId() == ((Person)obj).getId()){
+           return true;
+       }
+       return false;
+    }
+
+
 }
